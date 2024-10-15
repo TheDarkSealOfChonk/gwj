49f5660ba9b2e847d0ae5b96f04b3185ca41e5f8
dulsithakalanka <dulsithakalanka@gmail.com>
2024-10-14 18:52:12 -0700

    hello world shader

--- player/player.gd
+++ player/player.gd
@@ -2,15 +2,15 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-
+#Sounds
 @onready var SMALL_SCARE_SOUNDS = [
 	$SmallScareSounds/Sound01,
 	$SmallScareSounds/Sound02,
 	$SmallScareSounds/Sound03,
 ]
 
-@onready var small_scare_hit_dealer: HitDealer = $HitDealer
-@onready var small_scare_hit_dealer_collision_shape: CollisionShape2D = $HitDealer/CollisionShape
+@onready var hit_dealer: HitDealer = $HitDealer
+@onready var hit_dealer_collision_shape: CollisionShape2D = $HitDealer/CollisionShape
 @onready var sprite: AnimatedSprite2D = $Sprite
 @onready var da_pig: Sprite2D = $DaPig
 @onready var settings_manager: SettingsManager = $/root/Main/SettingsManager
@@ -18,15 +18,16 @@ const SPEED = 300.0
 var scare_on_cooldown = false
 
 func _physics_process(delta: float) -> void:
-	small_scare_hit_dealer.rotation = global_position.angle_to_point(get_global_mouse_position()) + PI
+	hit_dealer.rotation = global_position.angle_to_point(get_global_mouse_position()) + PI
 	
 	if Input.is_action_just_pressed("shake") && !scare_on_cooldown:
 		if settings_manager.is_oink_mode_enabled():
 			da_pig.visible = true
 			get_tree().create_timer(1).timeout.connect(func(): da_pig.visible = false)
-		small_scare_hit_dealer_collision_shape.disabled = false
+		hit_dealer_collision_shape.disabled = false
+		sprite.animation = "scare"
 		scare_on_cooldown = true
-		get_tree().create_timer(0.2).timeout.connect(func(): small_scare_hit_dealer_collision_shape.disabled = true)
+		get_tree().create_timer(0.2).timeout.connect(func(): hit_dealer_collision_shape.disabled = true; sprite.animation = "idle")
 		get_tree().create_timer(0.6).timeout.connect(func(): scare_on_cooldown = false)
 		
 		SMALL_SCARE_SOUNDS[randi_range(0, 2)].play()
@@ -36,13 +37,21 @@ func _physics_process(delta: float) -> void:
 	var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down").normalized()
 	
 	if direction:
-		sprite.update_walking(true)
-		sprite.update_direction_from_rad(Vector2().angle_to_point(direction))
-		sprite.flush_to_animation()
 		velocity = direction * SPEED
+		
 	else:
-		sprite.update_walking(false)
-		sprite.flush_to_animation()
 		velocity = Vector2(move_toward(velocity.x, 0, SPEED), move_toward(velocity.y, 0, SPEED))
-	
+	#print(direction)
+	match direction:
+		Vector2(0,-1):
+			sprite.animation="walkBack"
+		Vector2(0,1):
+			sprite.animation="walkForward"
+		Vector2(-1,0):
+			sprite.animation ="walkLeft"
+		Vector2(1,0):
+			sprite.animation="walkRight"
+		_:
+			sprite.animation = "idle"
+		
 	move_and_slide()
